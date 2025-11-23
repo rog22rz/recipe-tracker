dfdce8294cc77f3c6f6d8f1ac87e76fa5608e7b3
Roger Zhang <roger.zhang@mail.mcgill.ca>
2025-11-23 08:17:47 -0500

    Adding db model and routes

---
+++ db/migrate/20251123131339_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration[8.1]
+  def change
+    create_table :recipes do |t|
+      t.string :tile
+      t.text :description
+      t.text :instructions
+
+      t.timestamps
+    end
+  end
+end
